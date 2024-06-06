911ffc24e6ad45f6a09d094677ce7b5ca7d66825
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-06 16:35:48 -0300

    m

--- front/src/app/detail/page.tsx
+++ front/src/app/detail/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DetailProduct from './[id]/page'
 import { IProduct } from '@/helpers/types'
 
-const DetailPage: React.FC<{ id: string }> = ({ id }: { id: string }) => {
+const DetailPage = ({id}: {id: string}) => {
   return (
     <div>
         <DetailProduct params={{ id }} />
