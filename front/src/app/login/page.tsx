57687c75cb63d9214fa7abecf5e565f9d1e3e8b3
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-06 17:38:09 -0300

    mod en loginpage

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import Login from "@/components/Login/Login";
+import { UserSession } from "@/helpers/types/userSession";
 import React, { useState } from 'react'
 
 const LoginPage: React.FC = () => {
-  const [token, setToken] = useState<string | null>(localStorage.getItem("userToken") ?? null);
-
+  const [token, setToken] = useState<string | null>(localStorage.getItem("userSession") ?? null);
+  
   return (
     <Login token={token} setToken={setToken}/>
   )
