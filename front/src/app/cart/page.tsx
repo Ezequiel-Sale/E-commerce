f317a93d0142e3d3e269037d17ee4adb17f74055
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-06 15:53:47 -0300

    modificacion de nombre de componente page

--- front/src/app/cart/page.tsx
+++ front/src/app/cart/page.tsx
@@ -2,10 +2,10 @@ import Cart from '@/components/Cart/Cart'
 import { Product } from '@/helpers/types/Product'
 import React from 'react'
 
-const page = ({id, name, description, price, stock, image, categoryId}: Product) => {
+const Page = ({id, name, description, price, stock, image, categoryId}: Product) => {
   return (
     <Cart id={id} name={name} description={description} price={price} stock={stock} image={image} categoryId={categoryId}/>
   )
 }
 
-export default page
+export default Page
