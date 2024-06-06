40b18d9e478c7bc07b7af0bcbe3dac9de7cb152f
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-06 18:00:32 -0300

    cambio

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Login from '@/components/Login/Login'
-import { LoginProps } from '@/helpers/types/loginrops'
+import React from "react";
+import Login from "@/components/Login/Login";
+import { LoginProps } from "@/helpers/types/loginrops";
 
-const page: React.FC<LoginProps> = ({token, setToken}: LoginProps) => {
+const page: React.FC<LoginProps> = ({ token, setToken }) => {
   return (
     <>
-    <Login token={token} setToken={setToken}/>
+      <Login token={token} setToken={setToken} />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
