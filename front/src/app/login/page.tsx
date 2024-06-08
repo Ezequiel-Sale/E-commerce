d44f1c7dbb57404601a5bb485ef80a2d7a7a45ad
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-08 06:16:40 -0300

    se modifico nuevamente el componente login

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Login from "@/components/Login/Login";
 
-const page = ({ token, setToken }: { token: string | null; setToken: (token: string) => void }) => {
+const page = ({ token, setToken }: any) => {
   return (
     <>
       <Login token={token} setToken={setToken} />
