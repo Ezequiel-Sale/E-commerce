54db6ce614d728acd4c0ebefc580abe7a6fe0d9d
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-10 07:19:55 -0300

    se agrego un loading en todas las vistas

---
+++ front/src/app/dashboard/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <div className=" flex justify-center items-center mt-24 h-[100vh]">
+    <div className="loader">
+      <div className="loader-square"></div>
+      <div className="loader-square"></div>
+      <div className="loader-square"></div>
+      <div className="loader-square"></div>
+      <div className="loader-square"></div>
+      <div className="loader-square"></div>
+      <div className="loader-square"></div>
+    </div>
+    </div>
+  );
+}
