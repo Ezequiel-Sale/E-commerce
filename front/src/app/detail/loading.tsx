8e2254cb9174448c0a9cff5f04aa96464d169d86
Ezequiel Sale <ezequielsale123@gmail.com>
2024-06-10 07:23:38 -0300

    se modifico el estilo del loader

--- front/src/app/detail/loading.tsx
+++ front/src/app/detail/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className=" flex justify-center items-center mt-24 h-[100vh]">
+    <div className=" flex justify-center items-center h-[100vh]">
     <div className="loader">
       <div className="loader-square"></div>
       <div className="loader-square"></div>
